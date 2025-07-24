ec7c6351b3f0d5635dfbe6b9e718692f36b0fef5
Rudraksh Gupta <rudrakshgupta40@gmail.com>
2025-07-24 07:18:06 +0530

    Vercel Analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { Analytics } from "@vercel/analytics/react"
+// import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient();
 
@@ -22,7 +22,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
-    <Analytics />
+    {/* <Analytics /> */}
   </QueryClientProvider>
 );
 
